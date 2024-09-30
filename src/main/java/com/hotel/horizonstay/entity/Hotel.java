108883dbeedf5f61233bc58fece8b72f74237531
Tharindu Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2024-09-30 12:02:10 +0530

    feat(entity): add hotel entity

---
+++ src/main/java/com/hotel/horizonstay/entity/Hotel.java
@@ -0,0 +1,30 @@
+package com.hotel.horizonstay.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "hotels")
+@Data
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long hotelID;
+
+    private String hotelName;
+    private String hotelDescription;
+    private String hotelContactNumber;
+    private String hotelCity;
+    private String hotelCountry;
+    private String hotelEmail;
+    private Float hotelRating;
+
+    @ElementCollection
+    private List<String> hotelImages;
+
+}
