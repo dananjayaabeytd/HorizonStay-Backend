5ea977bd3f826c7c25e39a42b095b9f3be5af2b4
Tharindu Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2024-09-30 11:40:04 +0530

    project initialized

---
+++ src/main/java/com/hotel/horizonstay/HorizonstayApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.horizonstay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizonstayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorizonstayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel/horizonstay/HorizonstayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.horizonstay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorizonstayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
